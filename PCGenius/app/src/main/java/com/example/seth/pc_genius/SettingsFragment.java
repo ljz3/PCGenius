3f8f435c947193938867b9df1e6ac35b866a0435
unknown <kevin.ottawa@hotmail.com>
2019-03-26 14:52:41 -0400

    Menu Navigation

--- PCGenius/app/src/main/java/com/example/seth/pc_genius/SettingsFragment.java
+++ PCGenius/app/src/main/java/com/example/seth/pc_genius/SettingsFragment.java
@@ -1,20 +1,18 @@
 package com.example.seth.pc_genius;
 
 import android.os.Bundle;
-
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BuildsFragment extends Fragment {
+public class SettingsFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_builds, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
 
