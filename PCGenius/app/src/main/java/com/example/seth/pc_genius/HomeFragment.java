b70509b58462bb6ea1624e32979f9726e0fb509e
PlayingAlone <42653876+PlayingAlone@users.noreply.github.com>
2019-03-29 12:25:29 -0400

    ListView Home

--- PCGenius/app/src/main/java/com/example/seth/pc_genius/HomeFragment.java
+++ PCGenius/app/src/main/java/com/example/seth/pc_genius/HomeFragment.java
@@ -8,12 +8,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    ListView listView;
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8",
+    };
+
+
+    int[] listviewImage = new int[]{
+            R.drawable.lock_icon, R.drawable.lock_icon, R.drawable.lock_icon, R.drawable.lock_icon,
+            R.drawable.lock_icon, R.drawable.lock_icon, R.drawable.lock_icon, R.drawable.lock_icon,
+    };
+
+    String[] listviewShortDescription = new String[]{
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+    };
 
     @Nullable
     @Override
@@ -27,11 +44,23 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Home");
-        listView = getView().findViewById(R.id.list);
-        ArrayList<String> StringArray = new ArrayList<>();
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.simple_layout_,StringArray);
+        for (int i = 0; i < 8; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_discription", listviewShortDescription[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
 
-        listView.setAdapter(adapter);
+
+        String[] from = {"listview_image", "listview_title", "listview_discription"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.listview_activity, from, to);
+        ListView androidListView = getView().findViewById(R.id.list);
+        androidListView.setAdapter(simpleAdapter);
     }
-}
+    }
+
