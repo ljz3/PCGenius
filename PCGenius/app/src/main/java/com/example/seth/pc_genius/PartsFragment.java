5a0036e7055bf604a4a8b2e1e33d15461a55030b
PlayingAlone <42653876+PlayingAlone@users.noreply.github.com>
2019-04-02 13:53:23 -0400

    Update PartsFragment.java

--- PCGenius/app/src/main/java/com/example/seth/pc_genius/PartsFragment.java
+++ PCGenius/app/src/main/java/com/example/seth/pc_genius/PartsFragment.java
@@ -8,22 +8,59 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class PartsFragment extends Fragment{
 
-    @Nullable
-    @Override
+
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4", "ListView Title 5",
+            "ListView Title 6", "ListView Title 7", "ListView Title 8", "ListView Title 9", "ListView Title 10",
+    };
+
+
+    int[] listviewImage = new int[]{
+            R.drawable.image_icon, R.drawable.image_icon, R.drawable.image_icon, R.drawable.image_icon, R.drawable.image_icon,
+            R.drawable.image_icon, R.drawable.image_icon, R.drawable.image_icon, R.drawable.image_icon, R.drawable.image_icon,
+    };
+
+    String[] listviewShortDescription = new String[]{
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+    };
+
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_parts, container, false);
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
-        getActivity().setTitle("Parts");
+        getActivity().setTitle("Home");
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_discription", listviewShortDescription[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
+
+
+        String[] from = {"listview_image", "listview_title", "listview_discription"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.listview_activity, from, to);
+        ListView androidListView = getView().findViewById(R.id.list_parts);
+        androidListView.setAdapter(simpleAdapter);
     }
 }
