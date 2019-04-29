d93b3a4fcd4087b4231ac7d46e73f481e1cb384f
ljz3 <kevin.ottawa@hotmail.com>
2019-04-29 00:05:22 -0400

    im crying

--- PCGenius/app/src/main/java/com/example/seth/pc_genius/SavedPartsScreen/PartsFragment.java
+++ PCGenius/app/src/main/java/com/example/seth/pc_genius/SavedPartsScreen/PartsFragment.java
@@ -20,18 +20,25 @@ import com.example.seth.pc_genius.PartObject.Part;
 import com.example.seth.pc_genius.PartObject.PartAdapter;
 import com.example.seth.pc_genius.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PartsFragment extends Fragment {
     protected List<Part> list;
+    private List<Part> parts = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setTitle("Parts");
         list = new ArrayList<>();
-        PartsFragmentContents.initPartsListParts(list, getContext());
+        readPartData();
+        PartsFragmentContents.initPartsListParts(list, getContext(), parts);
 
         PartAdapter adapter = new PartAdapter(getActivity(), -1, list);
         View view = inflater.inflate(R.layout.fragment_parts, container, false);
@@ -59,6 +66,41 @@ public class PartsFragment extends Fragment {
         });
 
         return view;
+
+    }
+
+    private void readPartData() {
+        InputStream is = getResources().openRawResource(R.raw.gpu);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+     //   try {
+       //     while ((line = reader.readLine()) != null) {
+                // Split by ','
+                String[] tokens = line.split(",");
+
+                //Read the data
+                //Type,Part Number,Brand,Model,Rank,Benchmark,Samples,URL
+                Part part = new Part();
+                part.setmType(tokens[0]);
+                part.setmNum(tokens[1]);
+                part.setmBrand(tokens[2]);
+                part.setmModel(tokens[3]);
+                part.setmRank(Integer.getInteger(tokens[4]));
+                part.setmBenchmark(Integer.getInteger(tokens[5]));
+                part.setmSamples(Integer.getInteger(tokens[6]));
+                part.setmURL(tokens[7]);
+
+                parts.add(part);
+
+        //    }
+      //  } catch (IOException e) {
+    //        Log.wtf("MyActivity", "Error reading data file on line " + line, e);
+    //        e.printStackTrace();
+     //   }
+
 
     }
 
--- PCGenius/app/src/main/java/com/example/seth/pc_genius/SavedPartsScreen/PartsFragmentContents.java
+++ PCGenius/app/src/main/java/com/example/seth/pc_genius/SavedPartsScreen/PartsFragmentContents.java
@@ -16,49 +16,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PartsFragmentContents extends Fragment {
-    private static List<Part> parts = new ArrayList<>();
 
-    public void initPartsListParts(List<Part> list, Context context) {
-    readPartData();
+    public static void initPartsListParts(List<Part> list, Context context, List<Part> parts) {
 
-    list = parts;
-    }
-
-
-    private void readPartData() {
-        InputStream is = getResources().openRawResource(R.raw.gpu);
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(is, Charset.forName("UTF-8"))
-        );
-
-        String line = "";
-        try {
-            while ((line = reader.readLine()) != null) {
-                // Split by ','
-                String[] tokens = line.split(",");
-
-                //Read the data
-                //Type,Part Number,Brand,Model,Rank,Benchmark,Samples,URL
-                Part part = new Part();
-                part.setmType(tokens[0]);
-                part.setmNum(tokens[1]);
-                part.setmBrand(tokens[2]);
-                part.setmModel(tokens[3]);
-                part.setmRank(Integer.getInteger(tokens[4]));
-                part.setmBenchmark(Integer.getInteger(tokens[5]));
-                part.setmSamples(Integer.getInteger(tokens[6]));
-                part.setmURL(tokens[7]);
-
-                parts.add(part);
-
-            }
-        } catch (IOException e) {
-            Log.wtf("MyActivity", "Error reading data file on line " + line, e);
-            e.printStackTrace();
+        for(Part part: parts){
+            list.add(part);
         }
 
-
+      //  list.add(new Part("random", "Motherboard", 199.99, R.drawable.image_icon));
     }
-
 
 }
